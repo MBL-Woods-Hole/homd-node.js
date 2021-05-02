37a2eb5c4bddf0ccf43ac95bb24738dd3389ed7d
Andrew Voorhis <avoorhis@mbl.edu>
2021-05-02 13:27:24 -0400

    virome and fix help

--- config/database.js
+++ config/database.js
@@ -5,25 +5,35 @@ var fs    = require('fs-extra');
 var db_config_file = './config/db-connection.js';
 eval(fs.readFileSync(db_config_file).toString());
 
-
 exports.taxon_pool      =    mysql.createPool({
     connectionLimit : 100, //important
-    host     : db_config_tax.host,
-    user     : db_config_tax.user,
-    password : db_config_tax.password,
-    socketPath : db_config_tax.socketPath,
-    database : db_config_tax.database,
+    host     : db_config.host,
+    user     : db_config.user,
+    password : db_config.password,
+    socketPath : db_config.socketPath,
+    database : db_config.database,
     debug    :  false
 });
 
-exports.genome_pool      =    mysql.createPool({
-    connectionLimit : 100, //important
-    host     : db_config_gen.host,
-    user     : db_config_gen.user,
-    password : db_config_gen.password,
-    socketPath : db_config_gen.socketPath,
-    database : db_config_gen.database,
-    debug    :  false
-});
 
+// exports.taxon_pool      =    mysql.createPool({
+//     connectionLimit : 100, //important
+//     host     : db_config_tax.host,
+//     user     : db_config_tax.user,
+//     password : db_config_tax.password,
+//     socketPath : db_config_tax.socketPath,
+//     database : db_config_tax.database,
+//     debug    :  false
+// });
+// 
+// exports.genome_pool      =    mysql.createPool({
+//     connectionLimit : 100, //important
+//     host     : db_config_gen.host,
+//     user     : db_config_gen.user,
+//     password : db_config_gen.password,
+//     socketPath : db_config_gen.socketPath,
+//     database : db_config_gen.database,
+//     debug    :  false
+// });
+// 
 
