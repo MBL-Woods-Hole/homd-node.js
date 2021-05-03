ea9e9084edb509c904df1c90902662600f37cc53
Andrew Voorhis <avoorhis@mbl.edu>
2021-05-03 10:35:52 -0400

    vtable

--- routes/routes_virome.js
+++ routes/routes_virome.js
@@ -11,32 +11,86 @@ const queries = require(app_root + '/routes/queries')
 router.get('/', (req, res) => {
   console.log('in virome')
   helpers.accesslog(req, res)
-
-  res.render('pages/virome/index', {
-		title: 'HOMD :: Human Oral Virome Database',
-		config :  JSON.stringify({hostname:CFG.HOSTNAME, env:CFG.ENV}),
-		ver_info: JSON.stringify({rna_ver:C.rRNA_refseq_version, gen_ver:C.genomic_refseq_version}),
-		
+  fs.readFile(path.join(CFG.PATH_TO_DATA, C.virome_file), 'utf8', (err, data) => {
+    	if (err)
+      		console.log(err)
+    	else{
+         // add virome to global constants
+          C.virome_lookup = JSON.parse(data)
+          res.render('pages/virome/index', {
+                title: 'HOMD :: Human Oral Virome Database',
+                config :  JSON.stringify({hostname:CFG.HOSTNAME, env:CFG.ENV}),
+                ver_info: JSON.stringify({rna_ver:C.rRNA_refseq_version, gen_ver:C.genomic_refseq_version}),
+        
+          });
+     }
   });
-  
 });
 
 router.get('/virus_table', (req, res) => {
   console.log('in virus table')
   helpers.accesslog(req, res)
 
-  fs.readFile(path.join(CFG.PATH_TO_DATA, C.virome_file), 'utf8', (err, data) => {
-    	if (err)
-      		console.log(err)
-    	else{
-          data = JSON.parse(data)
+ //  fs.readFile(path.join(CFG.PATH_TO_DATA, C.virome_file), 'utf8', (err, data) => {
+//     	if (err)
+//       		console.log(err)
+//     	else{
+//           data = JSON.parse(data)
 		  res.render('pages/virome/virometable', {
 				title: 'HOMD :: Human Oral Virome Database',
 				config :  JSON.stringify({hostname:CFG.HOSTNAME, env:CFG.ENV}),
 				ver_info: JSON.stringify({rna_ver:C.rRNA_refseq_version, gen_ver:C.genomic_refseq_version}),
-				vdata:    JSON.stringify(data),
+				vdata:    JSON.stringify(C.virome_lookup),
 		  });
-        }
-  });
+//         }
+//   });
 });
+
+
+router.get('/dld_ttable', (req, res) => {
+	helpers.accesslog(req, res)
+	console.log(req.body)
+	let myurl = url.parse(req.url, true);
+  	let type = myurl.query.type;
+	// type = browser text or excel
+	var table_tsv = create_table('ttable','browser')
+	if(type === 'browser'){
+	    res.set('Content-Type', 'text/plain');  // <= important - allows tabs to display
+	}else if(type === 'text'){
+	    res.set({"Content-Disposition":"attachment; filename=\"HOMD_taxon_table.txt\""});
+	}else if(type === 'excel'){
+	    res.set({"Content-Disposition":"attachment; filename=\"HOMD_taxon_table.xls\""});
+	}else{
+	    // error
+	    console.log('Download table format ERROR')
+	}
+	res.send(table_tsv)
+	res.end()
+});
+
+////////////////////////////////
+////////////////////////////////
+function create_table(source, type) {
+
+    if(source === 'ttable' && type === 'browser'){
+       
+        var headers_row = ["Virus-ID","Assembly.NCBI","Accession.NCBI","Family.NCBI","Genus.NCBI","Species.NCBI","Molecule_Type.NCBI","Sequence_Type.NCBI","Host.NCBI","Isolation_Source.NCBI","Collection_Date.NCBI","BioSample.NCBI","Genbank_Title.NCBI"]
+        
+        txt =  headers_row.join('\t')
+        
+        for(vid in C.virome_lookup){
+            obj = C.virome_lookup[vid]
+               
+            
+               //console.log(o2)
+               var r = [vid, obj.assembly_ncbi, obj.sra_accession_ncbi, obj.family_ncbi, obj.genus_ncbi, obj.species_ncbi, obj.molecule_type_ncbi, obj.sequence_type_ncbi, obj.host_ncbi, obj.isolation_source_ncbi, obj.collection_date_ncbi, obj.biosample_ncbi, obj.genbank_title_ncbi]
+               row = r.join('\t')
+               txt += '\n'+row
+            
+        }
+    }   
+    //console.log(txt)
+    return txt
+}        
+        
 module.exports = router;
