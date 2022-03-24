6b4faca37e209a01c222be018356919afd37c80d
Andrew Voorhis <avoorhis@mbl.edu>
2022-03-24 12:56:11 -0400

    blast hit debug

--- routes/helpers/helpers.js
+++ routes/helpers/helpers.js
@@ -625,8 +625,9 @@ module.exports.parse_blast_query_xml = function parse_blast_query_xml(jsondata, 
         }else{
 			for(let n in json['BlastOutput']['BlastOutput_iterations']['Iteration']['Iteration_hits']){
 				hits = json['BlastOutput']['BlastOutput_iterations']['Iteration']['Iteration_hits'][n]
+				console.log('**hit**',n,hits)
 				for(let i in hits){
-			        console.log('**hit**',hits[i])
+			        
 			        homdhitid = hits[i].Hit_def.split(' ')[0]
 			        ret.hitid_ary.push(homdhitid)
 				}
