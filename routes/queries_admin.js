5af26c1db24067a46184946c6c1d4dcddb7c3bfa
Andrew Voorhis <avoorhis@mbl.edu>
2022-05-04 14:05:29 -0400

    account2

--- routes/queries_admin.js
+++ routes/queries_admin.js
@@ -4,7 +4,8 @@
 //////////////////////////
 module.exports.get_user_by_name = function get_user_by_name(uname, passwd) {
     var q = "SELECT user_id, username, email, institution, first_name, last_name, active, security_level,"
-     q += " encrypted_password, PASSWORD('"+passwd+"') as entered_pw, sign_in_count,"
+     //q += " encrypted_password, PASSWORD('"+passwd+"') as entered_pw, sign_in_count,"
+     q += " encrypted_password, CONCAT('*', UPPER(SHA1(UNHEX(SHA1('"+passwd+"'))))) as entered_pw, sign_in_count,"
      q += " DATE_FORMAT(current_sign_in_at,'%Y-%m-%d %T') as current_sign_in_at,last_sign_in_at FROM user"
      q += " WHERE username ='"+uname+"'"
     return q;
@@ -24,7 +25,8 @@ module.exports.insert_new_user = function insert_new_user(mysql_new_user) {
     var qInsertUser = "INSERT INTO user (username, encrypted_password, first_name, last_name, email, institution, active, sign_in_count, security_level, current_sign_in_at, last_sign_in_at,first_sign_in)";
     qInsertUser +=    " VALUES ('" + mysql_new_user.username +"', "+
                                    // helpers.generateHash(mysql_new_user.password) +"', '"+
-                                    "PASSWORD('"+mysql_new_user.password+"'),   '"+
+                                   // "PASSWORD('"+mysql_new_user.password+"'),   '"+
+                                    "CONCAT('*', UPPER(SHA1(UNHEX(SHA1('"+mysql_new_user.password+"'))))),   '"+
                                     mysql_new_user.firstname +"', '"+
                                     mysql_new_user.lastname +"', '"+
                                     mysql_new_user.email +"', '"+
