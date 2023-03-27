eea22c4f8333c7e3363013d9498457baf2101969
Andrew Voorhis <avoorhis@mbl.edu>
2023-03-27 12:31:07 -0400

    path to conn

--- app.js
+++ app.js
@@ -66,11 +66,11 @@ const help     = require('./routes/routes_help');
 
 
 // PRODUCTION: log every restart
-//if(CFG.ENV === 'production'){
+if(CFG.ENV === 'production'){
     const output = fs.createWriteStream('../homd-stats/restart.log', {flags : 'a'})
     const restart_logger = new console.Console(output)
     restart_logger.log('Restart on '+helpers.timestamp(false))
-//}
+}
 
 
 //passportConfig(passport, TDBConn);
