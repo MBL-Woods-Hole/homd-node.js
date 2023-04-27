6a0d641ced8005015e33e0d6a033495c438de753
Andrew Voorhis <avoorhis@mbl.edu>
2023-04-27 10:06:56 -0400

    first anno3

--- public/js/search.js
+++ public/js/search.js
@@ -80,7 +80,7 @@ function get_annotations_counts(intext){
     document.getElementById('prokka_count_div').innerHTML = '<img id="" class="loader-gif" align="center" src="/images/row-of-blocks-loader-animation.gif"> Searching'
     document.getElementById('ncbi_count_div').innerHTML = '<img id="" class="loader-gif" align="center" src="/images/row-of-blocks-loader-animation.gif"> Searching'
     xmlhttp.open("POST", "/get_annotations_counts_NEW", true);
-    xmlhttp.timeout = 5000;
+    xmlhttp.timeout = 1200000;  // 10,000; timeout in ms, 10 seconds
     xmlhttp.setRequestHeader("Content-type","application/json");
     xmlhttp.onreadystatechange = function() {
       console.log('readystate: ',xmlhttp.readyState)
