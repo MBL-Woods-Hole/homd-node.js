afa24bc35880bed17c684f32dc8e0281e5a26ae8
avoorhis <avoorhis@mbl.edu>
2023-05-02 16:43:01 -0400

    search

--- public/js/search.js
+++ public/js/search.js
@@ -1,79 +1,5 @@
 
-// function get_annotations_counts_ncbi(intext){
-//   var xmlhttp = new XMLHttpRequest();
-//   var html =''
-//   var args = {intext: intext, anno_type: 'ncbi'}
-//   //document.getElementById('prokka_count_div').innerHTML = '<img id="" class="loader-gif" align="center" src="/images/row-of-blocks-loader-animation.gif"> Searching'
-//   document.getElementById('ncbi_count_div').innerHTML = '<img id="" class="loader-gif" align="center" src="/images/row-of-blocks-loader-animation.gif"> Searching'
-//   xmlhttp.open("POST", "/get_annotations_counts_ncbi", true);
-//   xmlhttp.setRequestHeader("Content-type","application/json");
-//   xmlhttp.onreadystatechange = function() {
-//       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//         var resp = JSON.parse(xmlhttp.responseText);
-//         //console.log('get_annotations_counts NCBI')
-//         console.log('ncbi',resp)
-//         // [genome_count,gene_count]
-//         if(parseInt(resp[0]) === 0){
-//             html += "0"
-//         }else{
-//             if(parseInt(resp[0]) === 1){
-//                 html += resp[1]+' gene(s) in 1 genome '
-//             }else{
-//                 html += resp[1]+' gene(s) in '+resp[0]+' genomes '
-//             }
-//             html += "--<span class='search_link' onclick=\"anno_search('"+intext+"','ncbi')\">n show results</span>--"
-//             
-//         }
-//         document.getElementById('ncbi_count_div').innerHTML = html
-//         
-//         // if(parseInt(resp[2]) === 0){
-// //             html2 = "0"
-// //         }else{
-// //             if(parseInt(resp[2]) === 1){
-// //                 html2 = resp[3]+' gene(s) in 1 genome '
-// //             }else{
-// //                 html2 = resp[3]+' gene(s) in '+resp[2]+' genomes '
-// //             }
-// //             html2 += "--<span class='search_link' onclick=\"anno_search('"+intext+"','ncbi')\">show results</span>--"
-// //         }
-// //         document.getElementById('ncbi_count_div').innerHTML = html2
-//       }
-//   }
-//   xmlhttp.send(JSON.stringify(args));
-// }
 
-// function get_annotations_counts_prokka(intext){
-//   var xmlhttp = new XMLHttpRequest();
-//   var html =''
-//   var args = {intext: intext, anno_type: 'prokka'}
-//   document.getElementById('prokka_count_div').innerHTML = '<img id="" class="loader-gif" align="center" src="/images/row-of-blocks-loader-animation.gif"> Searching'
-//   //document.getElementById('ncbi_count_div').innerHTML = '<img id="" class="loader-gif" align="center" src="/images/row-of-blocks-loader-animation.gif"> Searching'
-//   xmlhttp.open("POST", "/get_annotations_counts_prokka", true);
-//   xmlhttp.setRequestHeader("Content-type","application/json");
-//   xmlhttp.onreadystatechange = function() {
-//       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//         var resp = JSON.parse(xmlhttp.responseText);
-//         //console.log('get_annotations_counts PROKKA')
-//         console.log('prokka',resp)
-//         // [genome_count,gene_count]
-//         if(parseInt(resp[0]) === 0){
-//             html += "0"
-//         }else{
-//             if(parseInt(resp[0]) === 1){
-//                 html += resp[1]+' gene(s) in 1 genome '
-//             }else{
-//                 html += resp[1]+' gene(s) in '+resp[0]+' genomes '
-//             }
-//             html += "--<span class='search_link' onclick=\"anno_search('"+intext+"','prokka')\">p show results</span>--"
-//             
-//         }
-//         document.getElementById('prokka_count_div').innerHTML = html
-//         
-//        
-//       }
-//   }
-//   xmlhttp.send(JSON.stringify(args));
-// }
 function get_annotations_counts(intext){
     var xmlhttp
     if (window.XMLHttpRequest) {
@@ -91,14 +17,14 @@ function get_annotations_counts(intext){
     xmlhttp.timeout = 1200000;  // 10,000; timeout in ms, 10 seconds
     xmlhttp.setRequestHeader("Content-type","application/json");
     xmlhttp.onreadystatechange = function() {
-      console.log('readystate: ',xmlhttp.readyState)
-      console.log('status: ',xmlhttp.status)
-      console.log('txt: ',xmlhttp.statusText)
+      //console.log('readystate: ',xmlhttp.readyState)
+      //console.log('status: ',xmlhttp.status)
+      //console.log('txt: ',xmlhttp.statusText)
       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        console.log('before parse')
+        //console.log('before parse')
         var resp = JSON.parse(xmlhttp.responseText);
-        console.log('get_annotations_counts')
-        console.log(resp)
+        //console.log('get_annotations_counts')
+        //console.log(resp)
         // [prokka_genome_count,prokka_gene_count,ncbi_genome_count,ncbi_gene_count]
         if(parseInt(resp[0]) === 0){
             html1 = "0"
