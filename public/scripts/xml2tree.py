a70b9162c6d0994f01ad76ebfcb2777baa467b14
avoorhis <avoorhis@mbl.edu>
2023-07-28 11:58:32 -0400

    finetune

--- public/scripts/xml2tree.py
+++ public/scripts/xml2tree.py
@@ -32,7 +32,15 @@ def run_unalingned(args):
     use_original_defline = False
     root = ET.parse(os.path.join(args.sourcedir,args.filename)).getroot()
     iter_count = 0
+    prog = 'unknown'
+    seqtype = 'nt'  #OR protein [default=nt]
+    for description in root.iter('BlastOutput_program'):
+        prog = description.text
+    print('PROG',prog)
+    if prog in ['blastp','blastx']:
+       seqtype = 'protein'
     for iteration in root.iter('Iteration'):
+        
         iter_count += 1
         if iter_count > 1:
             break
@@ -43,65 +51,86 @@ def run_unalingned(args):
                 for child in hit:
                     #print('child',child.tag)
                     if child.tag == 'Hit_def':
+                        # regex to find HMT
+                        
                         if use_original_defline:
                             # no whitespace
                             defline = '>'+child.text.replace(' ','')
                         else:
-                            child_text = child.text.replace(':','=').replace(',','_').replace('(','[').replace(')',']')
+                            child_text = child.text.replace(':','=').replace(',','_').replace('(','#').replace(')','#').replace('\n','')
+                            print('\nhitdef',child_text)
+                            hmt = 'HMT-000'
+                            hmtary = re.findall(r'HMT\-\d{3}', child_text)
+                            if len(hmtary):
+                                hmt = hmtary[0]
+                            print('myhmt',hmtary[0])
+                            
                             # REFSEQ:: 189AW006 | Kocuria atrinae | HMT-189 | Clone: AW006 | GB: AF385532
                             # GENOME::NCBI:   SEQF9758.1|UGNQ01000001.1 HMT-855 Kytococcus sedentarius NCTC11040
                             #         PROKKA: SEQF5240.1_02338 16S ribosomal RNA [HMT-188 Rothia aeria C6B]
-                            if child_text.startswith('SEQF'):
-                                # NCBI:GENOME:: SEQF9758.1|UGNQ01000001.1 HMT-855 Kytococcus sedentarius NCTC11040
-                                # PROKKA:GENOME SEQF5240.1_02338 16S ribosomal RNA [HMT-188 Rothia aeria C6B]
+                            if child_text.startswith('SEQF'):   # GENOMIC
+                                
                                 x = child_text.split()  # split on white space
 
                                 if '|' in x[0]:   # NCBI
-                                    # NCBI:NT::      SEQF9758.1|UGNQ01000001.1 HMT-855 Kytococcus sedentarius NCTC11040
-                                    # NCBI:PROTEIN:: SEQF3213.1|RRG14429.1 O-succinylbenzoic acid--CoA ligase [Porphyromonas gingivalis] [HMT-619 Porphyromonas gingivalis 381OKJP]
-                                    res = re.findall(r'\[.*?\]', child_text)
-                                    if len(res) == 0:
-                                        # NT
+                                    if seqtype == 'protein': # NCBI:PROTEIN:: 
+# SEQF3213.1|RRG14429.1 O-succinylbenzoic acid--CoA ligase [Porphyromonas gingivalis] [HMT-619 Porphyromonas gingivalis 381OKJP]
+# SEQF2456.1|ERI89612.1 putative CoA-substrate-specific enzyme activase [Clostridiales bacterium oral taxon 876 str. F0540] [HMT-876 Clostridiales [F-3][G-1] bacterium HMT 876 F0540]
+                                        res = re.findall(r'\[.*?\]', child_text)
+                                        print('bracket res',res)
+                                        try:
+                                            bracket = res[-1].lstrip('[').rstrip(']').split()  # choose last one if >1
+                                            species = bracket[1] + '_' + bracket[2]
+                                            species = species.replace('[','').replace(']','')
+                                        except:
+                                            bracket = res[0].lstrip('[').rstrip(']').split()  # choose last one if >1
+                                            species = bracket[1] + '_' + bracket[2]
+                                            species = species.replace('[','').replace(']','')
+                                        
+                                        defline = '>' + species + '|' + hmt + '|' + x[0]
+                                    else:  # NCBI:NT::      
+# SEQF9758.1|UGNQ01000001.1 HMT-855 Kytococcus sedentarius NCTC11040
+                                            # NT
                                         species = x[2] + '_' + x[3]
                                         species = species.replace('[','').replace(']','')
-                                        hmt = x[1]
-                                    else:
+
+                                        defline = '>' + species + '|' + hmt + '|' + x[0]
+                                else:   # PROKKA
+                                    if seqtype == 'protein':
+# PROKKA:PROTEIN:: 
+# SEQF3213.1_00735 2-succinylbenzoate--CoA ligase [HMT-619 Porphyromonas gingivalis 381OKJP]
+                                        res = re.findall(r'\[.*?\]', child_text)
                                         bracket = res[-1].lstrip('[').rstrip(']').split()  # choose last one if >1
                                         species = bracket[1] + '_' + bracket[2]
                                         species = species.replace('[','').replace(']','')
-                                        hmt = bracket[0]  
-                                    #defline = '>'+ x[0]+';'+x[1]
-                                    defline = '>' + species + '|' + hmt + '|'+x[0]
-                                else:   # PROKKA
-                                    # PROKKA:NT::      SEQF5240.1_02338 16S ribosomal RNA [HMT-188 Rothia aeria C6B]
-                                    # PROKKA:PROTEIN:: SEQF3213.1_00735 2-succinylbenzoate--CoA ligase [HMT-619 Porphyromonas gingivalis 381OKJP]
-                                    res = re.findall(r'\[.*?\]', child_text)
-                                    bracket = res[-1].lstrip('[').rstrip(']').split()  # choose last one if >1
-                                    species = bracket[1] + '_' + bracket[2]
-                                    species = species.replace('[','').replace(']','')
-                                    hmt = bracket[0]  
-                                    #print('res',res,bracket)
-                                    defline = '>' + species+'|' + hmt + '|' + x[0]
+                                         
+                                    else:  # PROKKA:NT::      
+# SEQF5240.1_02338 16S ribosomal RNA [HMT-188 Rothia aeria C6B]
+                                        res = re.findall(r'\[.*?\]', child_text)
+                                        bracket = res[-1].lstrip('[').rstrip(']').split()  # choose last one if >1
+                                        species = bracket[1] + '_' + bracket[2]
+                                        species = species.replace('[','').replace(']','') 
+                                        #print('res',res,bracket)
+                                        defline = '>' + species+'|' + hmt + '|' + x[0]
    
-                            else:
-                                # REFSEQ:: 189AW006 | Kocuria atrinae | HMT-189 | Clone: AW006 | GB: AF385532
+                            else:   # REFSEQ:: 189AW006 | Kocuria atrinae | HMT-189 | Clone: AW006 | GB: AF385532
+                                # always nucleotide
                                 x = [n.strip() for n in child_text.split('|')]
                                 species = x[1].replace(' ','_').replace('[','').replace(']','')
-                                hmt = x[2]
                                 #defline = '>'+ x[2]+';'+x[1].replace(' ','_')+';'+x[4].replace(' ','')
                                 defline = '>' + species + '|' + hmt + '|' + x[0]
                         print('Adding',defline)
                         fout.write(defline+'\n')
                         fasta_text += defline+'\n'
                 for hsp in hit.iter('Hsp'):
-                    print('hsp.tag',hsp.tag)
+                    #print('hsp.tag',hsp.tag)
                     for child in hsp:
                         if child.tag == 'Hsp_hseq':
                             sequence = str(child.text)
                             #print(sequence)
                             fout.write(sequence+'\n')
                             fasta_text += defline+'\n'
-                        
+    fout.close()
     
         
         
@@ -183,102 +212,102 @@ def run_unalingned(args):
 #                     #print(sequence)
 #                     fout.write(sequence+'\n')
 #                     fasta_text += defline+'\n'
-#     fout.close()
+#   fout.close()
 #     
 
     
-def run_alingnment(args): 
-    # https://taylor-lindsay.github.io/phylogenetics/
-    # https://github.com/taylor-lindsay/phylogenetics/blob/master/turtles/turtles.ipynb
-    args.maligned = args.outfilepath+"-aligned.aln"
-    infile = args.outfilepath
-    cmdls = [os.path.join(args.condabin,'muscle'), "-in",infile,'-out',args.maligned]  #,'-clw']
-    #print('Muscle:',' '.join(cmdls))
-    subprocess.call(cmdls)
+# def run_alingnment(args): 
+#     # https://taylor-lindsay.github.io/phylogenetics/
+#     # https://github.com/taylor-lindsay/phylogenetics/blob/master/turtles/turtles.ipynb
+#     args.maligned = args.outfilepath+"-aligned.aln"
+#     infile = args.outfilepath
+#     cmdls = [os.path.join(args.condabin,'muscle'), "-in",infile,'-out',args.maligned]  #,'-clw']
+#     #print('Muscle:',' '.join(cmdls))
+#     subprocess.call(cmdls)
+#     
+# def run_newick(args):
+#     args.maligned = args.outfilepath+"-aligned.aln"
+#     newickfile = os.path.join(args.sourcedir,"newick.tre")
+#     cmdls = [os.path.join(args.condabin,'fasttree'), "<",args.maligned,'>',newickfile]  #,'-clw']
+#     print('FastTree:',' '.join(cmdls))
+#     os.system(' '.join(cmdls))
     
-def run_newick(args):
-    args.maligned = args.outfilepath+"-aligned.aln"
-    newickfile = os.path.join(args.sourcedir,"newick.tre")
-    cmdls = [os.path.join(args.condabin,'fasttree'), "<",args.maligned,'>',newickfile]  #,'-clw']
-    print('FastTree:',' '.join(cmdls))
-    os.system(' '.join(cmdls))
     
-    
-def run_tree1(args):    
-    #print('HitCount',args.hit_count)
-    import matplotlib
-    import matplotlib.pyplot as plt
-    newickfile = os.path.join(args.sourcedir,"newick.tre")
-    my_tree = Phylo.read(newickfile, "newick")
-    
-    my_tree.rooted = True
-    
-    h = args.hit_count -3
-    w = 15
-    fig = plt.figure(figsize=(w, h), dpi=10) # create figure & set the size 
-    #plt.margins(0.2)
-    matplotlib.rc('font', size=10)              # fontsize of the leaf and node labels 
-    #matplotlib.rc('xtick', labelsize=10)       # fontsize of the tick labels
-    #matplotlib.rc('ytick', labelsize=10)       # fontsize of the tick labels
-    my_tree.ladderize()                        # optional view
-    axes = fig.add_subplot()
-    plt.subplots_adjust(right=0.7)
-    #xlim = axes.get_xbound()
-    #print('xlim',xlim)
-    #axes.set_xbound([xlim[0],xlim[1]+10])
-    
-    Phylo.draw(my_tree, label_func=get_label, axes=axes,  do_show=False) # myst be present and False
-    
-    pylab.savefig(os.path.join(args.sourcedir,'tree.svg'), format='svg',  dpi=1200)
+# def run_tree1(args):    
+#     #print('HitCount',args.hit_count)
+#     import matplotlib
+#     import matplotlib.pyplot as plt
+#     newickfile = os.path.join(args.sourcedir,"newick.tre")
+#     my_tree = Phylo.read(newickfile, "newick")
+#     
+#     my_tree.rooted = True
+#     
+#     h = args.hit_count -3
+#     w = 15
+#     fig = plt.figure(figsize=(w, h), dpi=100) # create figure & set the size 
+#     #plt.margins(0.2)
+#     matplotlib.rc('font', size=10)              # fontsize of the leaf and node labels 
+#     #matplotlib.rc('xtick', labelsize=10)       # fontsize of the tick labels
+#     #matplotlib.rc('ytick', labelsize=10)       # fontsize of the tick labels
+#     my_tree.ladderize()                        # optional view
+#     #axes = fig.add_subplot()
+#     #plt.subplots_adjust(right=0.7)
+#     #xlim = axes.get_xbound()
+#     #print('xlim',xlim)
+#     #axes.set_xbound([xlim[0],xlim[1]+10])
+#     
+#     Phylo.draw(my_tree, label_func=get_label,   do_show=False) # myst be present and False
+#     
+#     pylab.savefig(os.path.join(args.sourcedir,'tree.svg'), format='svg',  dpi=1200)
    
-def run_tree(args):    
-    #print('HitCount',args.hit_count)
-    import matplotlib
-    import matplotlib.pyplot as plt
-    newickfile = os.path.join(args.sourcedir,"newick.tre")
-    my_tree = Phylo.read(newickfile, "newick")
-    #print('tree',tree)
-    #sys.exit()
-    
-    with open(args.maligned,"r") as aln: 
-        alignment = AlignIO.read(aln,"fasta")  #"clustal") "fasta" "clustal"
-    
-    #subprocess.call(cmdls)
-
-    # print('ALN',alignment)
-#     calculator = DistanceCalculator('identity')
-#     distance_matrix = calculator.get_distance(alignment)
-#     print('DM:',distance_matrix)
+# def run_tree(args):    
+#     #print('HitCount',args.hit_count)
+#     import matplotlib
+#     import matplotlib.pyplot as plt
+#     newickfile = os.path.join(args.sourcedir,"newick.tre")
+#     my_tree = Phylo.read(newickfile, "newick")
+#     #print('tree',tree)
+#     #sys.exit()
 #     
-#     from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
-#     constructor = DistanceTreeConstructor(calculator)
+#     with open(args.maligned,"r") as aln: 
+#         alignment = AlignIO.read(aln,"fasta")  #"clustal") "fasta" "clustal"
 #     
-#     my_tree = constructor.build_tree(alignment)
-#     print('mytree',my_tree)
-#     #Phylo.write(my_tree, os.path.join(args.sourcedir,"newick.tre"), "newick")
-#     #print('NW',my_tree)
-    my_tree.rooted = True
-    #Phylo.write(my_tree, "turtle_tree.xml", "phyloxml")
-    #print(my_tree)
-    #Phylo.write(my_tree, "my_tree.xml", "phyloxml")
-    #import matplotlib
-    #import matplotlib.pyplot as plt
-    #fig = plt.figure(figsize=(13, 5), dpi=100)
-    h = args.hit_count -3
-    w = 20
-    fig = plt.figure(figsize=(w, h), dpi=150) # create figure & set the size 
-    
-    #matplotlib.rc('font', size=20)              # fontsize of the leaf and node labels 
-    #matplotlib.rc('xtick', labelsize=10)       # fontsize of the tick labels
-    #matplotlib.rc('ytick', labelsize=10)       # fontsize of the tick labels
-    my_tree.ladderize()                        # optional view
-    #plt.xticks([])
-    axes = fig.add_subplot(1, 1, 1)
-    Phylo.draw(my_tree, label_func=get_label, axes=axes,  do_show=False) # myst be present and False
-    #Phylo.draw(my_tree, label_func=get_label,  do_show=False)
-    #pylab.axis('off')
-    pylab.savefig(os.path.join(args.sourcedir,'tree2.svg'),format='svg', bbox_inches='tight', dpi=300)
-    #fig.savefig(os.path.join(args.sourcedir,"my_cladogram"))  # png
+#     #subprocess.call(cmdls)
+# 
+#     # print('ALN',alignment)
+# #     calculator = DistanceCalculator('identity')
+# #     distance_matrix = calculator.get_distance(alignment)
+# #     print('DM:',distance_matrix)
+# #     
+# #     from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+# #     constructor = DistanceTreeConstructor(calculator)
+# #     
+# #     my_tree = constructor.build_tree(alignment)
+# #     print('mytree',my_tree)
+# #     #Phylo.write(my_tree, os.path.join(args.sourcedir,"newick.tre"), "newick")
+# #     #print('NW',my_tree)
+#     my_tree.rooted = True
+#     #Phylo.write(my_tree, "turtle_tree.xml", "phyloxml")
+#     #print(my_tree)
+#     #Phylo.write(my_tree, "my_tree.xml", "phyloxml")
+#     #import matplotlib
+#     #import matplotlib.pyplot as plt
+#     #fig = plt.figure(figsize=(13, 5), dpi=100)
+#     h = args.hit_count -3
+#     w = 20
+#     fig = plt.figure(figsize=(w, h), dpi=150) # create figure & set the size 
+#     
+#     #matplotlib.rc('font', size=20)              # fontsize of the leaf and node labels 
+#     #matplotlib.rc('xtick', labelsize=10)       # fontsize of the tick labels
+#     #matplotlib.rc('ytick', labelsize=10)       # fontsize of the tick labels
+#     my_tree.ladderize()                        # optional view
+#     #plt.xticks([])
+#     axes = fig.add_subplot()
+#     Phylo.draw(my_tree, label_func=get_label, axes=axes,  do_show=False) # myst be present and False
+#     #Phylo.draw(my_tree, label_func=get_label,  do_show=False)
+#     #pylab.axis('off')
+#     pylab.savefig(os.path.join(args.sourcedir,'tree2.svg'),format='svg', bbox_inches='tight', dpi=300)
+#     #fig.savefig(os.path.join(args.sourcedir,"my_cladogram"))  # png
     
 def get_label(leaf):
     return leaf.name    
@@ -340,7 +369,7 @@ def run_nw_utils():
     f = open(svgfile, "w")
     #   nw_display -R 40 -s -v 20 -i 'opacity:0' -b 'visibility:hidden' -l 'font-family:san-serif' -w 1000 -W 6 tree.reroot.order.tre > ParB.svg
     #cmdls = [os.path.join(args.condabin,'nw_display'),'-b','visibility:hidden','-l','font-family:san-serif','-R','80','-s','-v','40','-w','1000','-W','5',rerootfile,'>',svgfile]  #newickfile+'|nw_order','-c','n','-','>',rerootfile]
-    cmdls = [os.path.join(args.condabin,'nw_display'),'-b','visibility:hidden','-l','font-family:san-serif','-R','80','-s','-v','40','-w','1000','-W','5',rerootfile]  #newickfile+'|nw_order','-c','n','-','>',rerootfile]
+    cmdls = [os.path.join(args.condabin,'nw_display'),'-b','visibility:hidden','-l','font-family:san-serif','-R','80','-s','-v','40','-w','1200','-W','10',rerootfile]  #newickfile+'|nw_order','-c','n','-','>',rerootfile]
     
     cmd = ' '.join(cmdls)
     print('nw_display:',cmd)
@@ -351,10 +380,13 @@ if __name__ == "__main__":
 
     usage = """
     USAGE:
-        -id/--directory   --- Full Path Input Directory ()
-        -mp/--muscle_path --- Full Path to muscle (including executable)
-        -ftp/--fasttree_path --- Full Path to fasttree
-        -c/--condabin  -- path to conda/bin contains: newick_utils,fasttree,mafft,muscle
+        Used in HOMD code in routes/index.js
+        
+        -id/--directory   --- Full Path Input Directory () [REQUIRED]
+        -mp/--muscle_path --- Full Path to muscle (including executable) (NOT USED Use -c conda bin only)
+        -ftp/--fasttree_path --- Full Path to fasttree (NOT USED Use -c conda bin only)
+        -c/--condabin  -- path to conda/bin contains: newick_utils,fasttree,mafft,muscle [REQUIRED On HOMD]
+        -p/--protein NOT USED (for fasttree)
     """
 
     parser = argparse.ArgumentParser(description="." ,usage=usage)
@@ -390,15 +422,15 @@ if __name__ == "__main__":
 #   ./replace_longname.pl ParB.svg $VERSION
     run_unalingned(args)
     #run_mafft()
-    run_muscle()
-    run_fasttree()
-    run_nw_utils()
-    
-    sys.exit()
+    run_muscle()   # fasta     -> alignment
+    run_fasttree() # alignment -> newick
+    run_nw_utils() # newick    -> tree.svg
     
     
-    run_alingnment(args)
-    run_newick(args)
-    run_tree1(args)
+    
+    
+    #run_alingnment(args)
+    #run_newick(args)
+    #run_tree1(args)
     print('PyScript Finished')
     
