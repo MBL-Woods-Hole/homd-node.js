794932b08a2c21d7e7f82d60495dab80605b82f4
avoorhis <avoorhis@mbl.edu>
2024-06-04 20:13:29 -0400

    note

--- public/constants.js
+++ public/constants.js
@@ -2,10 +2,13 @@ const CFG   = require(app_root + '/config/config');
 const constants = {};
 
 constants.access_log_format = ':ip - [:clfDate] ":method :url :protocol/:httpVersion" :statusCode :contentLength ":userAgent"'
-constants.access_logfile = CFG.LOG_DIR+'/homd-access.log'  // if you want a non-relative url use: config.js
+
+// logging for 1)anvio_post,2)jbrowse_ajax,3)taxa:ecology,4)genome:explorer
+constants.access_logfile = CFG.LOG_DIR+'/homd-access.log'  // JBROWSE if you want a non-relative url use: config.js
 
 constants.rRNA_refseq_version    = '15.23'
 constants.genomic_refseq_version = '10.1'
+constants.homd_taxonomy_version = '4.0'  // this is not used! yet
 constants.use_cluster = false
 constants.user_security_levels = {
     '1': 'Administrator',
@@ -14,22 +17,8 @@ constants.user_security_levels = {
 }
 //constants.ranks = ["domain", "phylum", "klass", "order", "family", "genus", "species"];
 constants.ranks = ["domain", "phylum", "klass", "order", "family", "genus", "species","subspecies"];
-// constants.phyla = [ 'Actinobacteria',
-//           'Bacteroidetes',
-//           'Chlamydiae',
-//           'Chlorobi',
-//           'Chloroflexi',
-//           'Euryarchaeota',
-//           'Firmicutes',
-//           'Fusobacteria',
-//           'Gracilibacteria (GN02)',
-//           'Proteobacteria',
-//           'Saccharibacteria (TM7)',
-//           'Spirochaetes',
-//           'SR1',
-//           'Synergistetes',
-//           'WPS-2'
-// ]
+
+  
 constants.names_w_text = {
   genera:['Actinomyces','Campylobacter','Corynebacterium','Gemella','Leptotrichia',
     'Porphyromonas','Pseudoleptotrichia','Schaalia','Tannerella','Aggregatibacter','Capnocytophaga',
@@ -52,29 +41,54 @@ constants.names_w_text = {
   ],
   species:['666']
 }
-constants.abundance_refs  = ['segata','eren_v1v3','eren_v3v5','dewhirst']
-//constants.abundance_order = ['BM',"KG",'HP','TD','PT','TH','SV',"SupP","SubP",'ST']
-constants.base_abundance_order       = ['SubP','SupP','KG','BM','HP','SV','TH','PT','TD'] //,'NS','ST']
-//constants.base_abundance_graphs      = ['SubP','SupP','KG','BM','HP','SV','TH','PT','TD','NS']
-//constants.segata_order =   constants.base_abundance_order.concat(['ST'])
-//constants.eren_order = constants.base_abundance_order.concat(['ST'])
-//constants.dewhirst_order  = constants.base_abundance_order.concat(['NS'])
-//constants.eren_order =     ['BM',"KG",'HP','TD','PT','TH','SV','SupP','SubP','ST']
-//constants.site_order      = ['SubP','SupP','KG','BM','HP','SV','TH','PT','TD','NS','ST']
-constants.abundance_names = {
-      'BM': "Buccal Mucosa (BM)",
-      "KG": "Keratinized Gingiva (KG)",
-      'HP': 'Hard Palate (HP)',
-      'TH': "Throat (TH)",
-      "PT": "Palatine Tonsils (PT)",
-      "TD": 'Tongue Dorsum (TD)',
-      "SV": "Saliva (SV)",
-      "SupP": "Supra-gingival Plaque (SupP)",
-      "SubP": "Sub-gingival Plaque (SubP)",
-      "NS": "Nasal (NS)",
-      "ST": "Stool (ST)"
-}  
+//constants.abundance_refs  = ['segata','eren_v1v3','eren_v3v5','dewhirst']
+constants.abundance_refs = ['eren_v1v3','eren_v3v5','dewhirst','hmp_refseq_v1v3','hmp_refseq_v3v5']
+constants.base_abundance_order     = ['SUBP','SUPP','AKE','BMU','HPA','SAL','THR','PTO','TDO','ANA','LRC','RRC','LAF','RAF','VIN','MVA','PFO','STO']
+constants.eren_abundance_order     = ['SUBP','SUPP','AKE','BMU','HPA','SAL','THR','PTO','TDO',                                                'STO']
+constants.dewhirst_abundance_order = ['SUBP','SUPP','AKE','BMU','HPA','SAL','THR','PTO','TDO','ANA']
+constants.hmp_metaphlan_abundance_order=['SUBP','SUPP','PERIO','AKE','BMU','HPA','SAL','THR','PTO','TDO','ANA','LRC','RRC','RAF','VIN','MVA','PFO','STO']
+constants.hmp_refseq_abundance_order   =['SUBP','SUPP','AKE','BMU','HPA','SAL','THR','PTO','TDO','ANA','LRC','RRC','LAF','RAF','VIN','MVA','PFO','STO']
 
+//CORRECT ORDER
+// SubP 
+// SupP
+// KG = AKE
+// BM = BMU
+// HP = HPA
+// SV = SAL
+// TH = THR
+// PT = PTO
+// TD = TDO
+// Nares = ANA
+// Retroauricular crease Left = LRC
+// Retroauricular crease Right = RRC
+// Anterior fossa Left =LAF
+// Anterior fossa Right = RAF
+// Vaginal introitus =VIN
+// mid-vagina = MVA
+// posterior fornix = PFO 
+// Stool = STO
+constants.abundance_names = { 
+'ANA':'Anterior Nares (nasal)',
+'AKE':'Attached Keratinized Gingiva (oral)',
+'BMU':'Buccal Mucosa (oral)',
+'HPA':'Hard Palate (oral)',
+'LAF':'L_Antecubital Fossa (skin)',
+'LRC':'L_Retroauricular Crease (skin)',
+'MVA':'Mid Vagina (vaginal)',
+'PTO':'Palatine Tonsils (oral)',
+'PFO':'Posterior Fornix (vaginal)',
+'RAF':'R_Antecubital Fossa (skin)',
+'RRC':'R_Retroauricular Crease (skin)',
+'SAL':'Saliva (oral)',
+'STO':'Stool (gut)',
+'SUBP':'Subgingival Plaque (oral)',
+'SUPP':'Supragingival Plaque (oral)',
+'THR':'Throat (oral)',
+'TDO':'Tongue Dorsum (oral)',
+'VIN':'Vaginal Introitus (vaginal)',
+'PERIO':'Periodontal (oral)'
+}
 //All the Capnocytophaga can be the same color;
 let capno_color = ['#a1ee0a','#98fb98','#00ff7f','#adff2f']  // 4 greens
 //both Corynebacterium matruchotii and Corynebacterium durum can be the same color;
@@ -95,67 +109,70 @@ let lepto2_color = '#96152e'
 //were very similar color or the same color to emphasize that we can't really tell them apart with this data. 
 let strep_color = ['#acbf60','#b0c24a','#a4c639','#8c9e5e','#9ab973'] // 5 green
 constants.plot_species = [  // to be put in db with species_id
-    {name:'Actinomyces graevenitzii',color:'lightsalmon'},
-    {name:'Actinomyces naeslundii',color:'#dc143c'},
-    {name:'Actinomyces oris',color:'#ff4040'},
-    {name:'Alloprevotella sp._HMT_473',color:'DarkViolet'},
-    {name:'Anaerococcus octavius',color: anaero_color[0]},
-    {name:'Anaerococcus sp._HMT_290',color: anaero_color[1]},
-    {name:'Campylobacter concisus',color:'#1f8ad5'},
-    {name:'Capnocytophaga gingivalis',color: capno_color[0]},
-    {name:'Capnocytophaga granulosa',color: capno_color[1]},
-    {name:'Capnocytophaga leadbetteri',color: capno_color[2]},
-    {name:'Capnocytophaga sputigena',color: capno_color[3]},
-    {name:'Corynebacterium accolens',color: nasal_coryne_color[0]},
-    {name:'Corynebacterium durum',color: coryne_color[0]},
-    {name:'Corynebacterium macginleyi',color: nasal_coryne_color[1]},
-    {name:'Corynebacterium matruchotii',color: coryne_color[1]},
-    {name:'Corynebacterium propinquum',color: nasal_coryne_color[2]},
-    {name:'Corynebacterium pseudodiphtheriticum',color: nasal_coryne_color[3]},
-    {name:'Corynebacterium tuberculostearicum',color: nasal_coryne_color[4]},
-    {name:'Cutibacterium acnes',color:'MediumSeaGreen'},
-    {name:'Fusobacterium nucleatum',color:'yellow'},
-    {name:'Fusobacterium periodonticum',color:'chartreuse'},
-    {name:'Gemella haemolysans',color:'#3bea0f'},
-    {name:'Granulicatella adiacens',color:'plum'},
-    {name:'Granulicatella elegans',color:'aliceblue'},
-    {name:'Haemophilus haemolyticus',color: haemo_color},
-    {name:'Haemophilus parainfluenzae',color:'#ffa500'},
-    {name:'Haemophilus sp._HMT_036',color: haemo_color},
-    {name:'Lautropia mirabilis',color:'#804e71'},
-    {name:'Leptotrichia buccalis',color: lepto1_color},
-    {name:'Leptotrichia wadei',color: lepto1_color},
-    {name:'Leptotrichia sp._HMT_417',color: lepto2_color},
-    {name:'Neisseria flavescens',color:'#a04115'},
-    {name:'Neisseria perflava',color:'#9a8bf0'},
-    {name:'Porphyromonas pasteri',color:'#87cefa'},
-    {name:'Porphyromonas sp._HMT_930',color:'moccasin'},
-    {name:'Prevotella melaninogenica',color:'#7b68ee'},
-    {name:'Prevotella oris',color:'#9400d3'},
-    {name:'Pseudoleptotrichia sp._HMT_221',color: lepto2_color},
-    {name:'Rothia aeria',color:'#4ddc9b'},
-    {name:'Rothia dentocariosa',color:'#e13ffa'},
-    {name:'Rothia mucilaginosa',color:'#f4f74a'},
-    {name:'Schaalia odontolytica',color:'#ff0000'},
-    {name:'Schaalia sp._HMT_172',color:'#ff0000'},
-    {name:'Schaalia sp._HMT_180',color:'#ff0000'},
-    {name:'Staphylococcus aureus',color: staph_color[0]},
-    {name:'Staphylococcus epidermidis',color: staph_color[1]},
-    {name:'Streptococcus australis',color:'#008080'},
-    {name:'Streptococcus cristatus',color:'#008080'},
-    {name:'Streptococcus infantis',color: strep_color[0]},
-    {name:'Streptococcus mitis',color: strep_color[1]},
-    {name:'Streptococcus oralis',color: strep_color[2]},
-    {name:'Streptococcus parasanguinis',color:'#3CBC3C'},
-    {name:'Streptococcus salivarius',color:'#50D050'},
-    {name:'Streptococcus sanguinis',color:'#008080'},
-    {name:'Streptococcus sp._HMT_074',color: strep_color[3]},
-    {name:'Streptococcus sp._HMT_423',color: strep_color[4]},
-    {name:'Veillonella atypica',color:'#ffcff1'},
-    {name:'Veillonella dispar',color:'#fddde6'},
-    {name:'Veillonella parvula',color:'#ffb6c1'},
-    {name:'Veillonella rogosae',color:'#fba0e3'},
-    {name:'Veillonella sp._HMT_780',color:'deeppink'}
+    {otid:'866',name:'Actinomyces graevenitzii',color:'lightsalmon'},
+    {otid:'176',name:'Actinomyces naeslundii',color:'#dc143c'},
+    {otid:'893',name:'Actinomyces oris',color:'#ff4040'},
+    {otid:'473',name:'Alloprevotella sp._HMT_473',color:'DarkViolet'},
+    {otid:'17',name:'Anaerococcus octavius',color: anaero_color[0]},
+    {otid:'290',name:'Anaerococcus sp._HMT_290',color: anaero_color[1]},
+    {otid:'575',name:'Campylobacter concisus',color:'#1f8ad5'},
+    {otid:'337',name:'Capnocytophaga gingivalis',color: capno_color[0]},
+    {otid:'325',name:'Capnocytophaga granulosa',color: capno_color[1]},
+    {otid:'329',name:'Capnocytophaga leadbetteri',color: capno_color[2]},
+    {otid:'775',name:'Capnocytophaga sputigena',color: capno_color[3]},
+    {otid:'19',name:'Corynebacterium accolens',color: nasal_coryne_color[0]},
+    {otid:'595',name:'Corynebacterium durum',color: coryne_color[0]},
+    {otid:'50',name:'Corynebacterium macginleyi',color: nasal_coryne_color[1]},
+    {otid:'666',name:'Corynebacterium matruchotii',color: coryne_color[1]},
+    {otid:'59',name:'Corynebacterium propinquum',color: nasal_coryne_color[2]},
+    {otid:'60',name:'Corynebacterium pseudodiphtheriticum',color: nasal_coryne_color[3]},
+    {otid:'77',name:'Corynebacterium tuberculostearicum',color: nasal_coryne_color[4]},
+    {otid:'530',name:'Cutibacterium acnes',color:'MediumSeaGreen'},
+    {otid:'698',name:'Fusobacterium nucleatum',color:'yellow'},
+    {otid:'201',name:'Fusobacterium periodonticum',color:'chartreuse'},
+    {otid:'626',name:'Gemella haemolysans',color:'#3bea0f'},
+    {otid:'534',name:'Granulicatella adiacens',color:'plum'},
+    {otid:'596',name:'Granulicatella elegans',color:'aliceblue'},
+    {otid:'851',name:'Haemophilus haemolyticus',color: haemo_color},
+    {otid:'718',name:'Haemophilus parainfluenzae',color:'#ffa500'},
+    {otid:'36',name:'Haemophilus sp._HMT_036',color: haemo_color},
+    {otid:'22',name:'Lautropia mirabilis',color:'#804e71'},
+    {otid:'563',name:'Leptotrichia buccalis',color: lepto1_color},
+    {otid:'222',name:'Leptotrichia wadei',color: lepto1_color},
+    {otid:'417',name:'Leptotrichia sp._HMT_417',color: lepto2_color},
+    {otid:'610',name:'Neisseria flavescens',color:'#a04115'},
+    {otid:'101',name:'Neisseria perflava',color:'#9a8bf0'},
+    {otid:'279',name:'Porphyromonas pasteri',color:'#87cefa'},
+    {otid:'930',name:'Porphyromonas sp._HMT_930',color:'moccasin'},
+    {otid:'469',name:'Prevotella melaninogenica',color:'#7b68ee'},
+    {otid:'311',name:'Prevotella oris',color:'#9400d3'},
+    {otid:'221',name:'Pseudoleptotrichia sp._HMT_221',color: lepto2_color},
+    {otid:'188',name:'Rothia aeria',color:'#4ddc9b'},
+    {otid:'587',name:'Rothia dentocariosa',color:'#e13ffa'},
+    {otid:'681',name:'Rothia mucilaginosa',color:'#f4f74a'},
+    {otid:'701',name:'Schaalia odontolytica',color:'#ff0000'},
+    {otid:'172',name:'Schaalia sp._HMT_172',color:'#ff0000'},
+    {otid:'180',name:'Schaalia sp._HMT_180',color:'#ff0000'},
+    {otid:'550',name:'Staphylococcus aureus',color: staph_color[0]},
+    {otid:'601',name:'Staphylococcus epidermidis',color: staph_color[1]},
+    {otid:'73',name:'Streptococcus australis',color:'#008080'},
+    {otid:'578',name:'Streptococcus cristatus',color:'#008080'},
+    {otid:'886',name:'Streptococcus cristatus',color:'#008080'},
+    {otid:'431',name:'Streptococcus infantis',color: strep_color[0]},
+    {otid:'638',name:'Streptococcus infantis',color: strep_color[0]},
+    {otid:'677',name:'Streptococcus mitis',color: strep_color[1]},
+    {otid:'707',name:'Streptococcus oralis',color: strep_color[2]},
+    {otid:'411',name:'Streptococcus parasanguinis',color:'#3CBC3C'},
+    {otid:'721',name:'Streptococcus parasanguinis',color:'#3CBC3C'},
+    {otid:'755',name:'Streptococcus salivarius',color:'#50D050'},
+    {otid:'758',name:'Streptococcus sanguinis',color:'#008080'},
+    {otid:'74',name:'Streptococcus sp._HMT_074',color: strep_color[3]},
+    {otid:'423',name:'Streptococcus sp._HMT_423',color: strep_color[4]},
+    {otid:'524',name:'Veillonella atypica',color:'#ffcff1'},
+    {otid:'160',name:'Veillonella dispar',color:'#fddde6'},
+    {otid:'161',name:'Veillonella parvula',color:'#ffb6c1'},
+    {otid:'158',name:'Veillonella rogosae',color:'#fba0e3'},
+    {otid:'780',name:'Veillonella sp._HMT_780',color:'deeppink'}
 ]   
 constants.tax_status_all =['named','unnamed','phylotype','lost','dropped','nonoralref'];
 constants.tax_status_on =['named','unnamed','phylotype','lost'];
@@ -168,7 +185,7 @@ constants.info_lookup_fn      = 'homdData-TaxonInfoLookup.json'
 constants.references_lookup_fn  = 'homdData-TaxonReferencesLookup.json'
 constants.taxcounts_fn          = 'homdData-TaxonCounts.json'
 constants.taxon_lookup_fn     = 'homdData-TaxonLookup.json'
-constants.lineage_lookup_fn   = 'homdData-TaxonLineagelookup.json'
+constants.lineage_lookup_fn   = 'homdData-TaxonLineagelookup.json' // supplied to create dict
 constants.tax_hierarchy_fn    = 'homdData-TaxonHierarchy.json'
 constants.refseq_lookup_fn    = 'homdData-TaxonRefSeqLookup.json'
 constants.phage_list_fn         = 'homdData-PhageList.json'
@@ -271,15 +288,15 @@ constants.all_genome_blastn_db_choices = {
            ext:'faa',filename: 'faa/ALL_genomes.faa' }
     ]
 }
-constants.species_w_subspecies = {
-    'Streptococcus parasanguinis': ['clade_411','clade_721'],
-    'Streptococcus infantis': ['clade_431','clade_638'],
-    'Streptococcus cristatus': ['clade_578','clade_886'],
-    'Streptococcus oralis': ['subsp._oralis', 'subsp._dentisani_clade_058', 'subsp._dentisani_clade_398', 'subsp._tigurinus_clade_070', 'subsp._tigurinus_clade_071'],
-    'Fusobacterium nucleatum': ['subsp._animalis', 'subsp._nucleatum', 'subsp._polymorphum', 'subsp._vincentii'],
-    'Limosilactobacillus reuteri': ['clade_818','clade_938'],  // no abundance data
-    'Peptostreptococcaceae_[G-7] [Eubacterium]_yurii': ['subsps._yurii_&_margaretiae','subsp._schtitka']
-}
+// constants.species_w_subspecies = {
+//     'Streptococcus parasanguinis': ['clade_411','clade_721'],
+//     'Streptococcus infantis': ['clade_431','clade_638'],
+//     'Streptococcus cristatus': ['clade_578','clade_886'],
+//     'Streptococcus oralis': ['subsp._oralis', 'subsp._dentisani_clade_058', 'subsp._dentisani_clade_398', 'subsp._tigurinus_clade_070', 'subsp._tigurinus_clade_071'],
+//     'Fusobacterium nucleatum': ['subsp._animalis', 'subsp._nucleatum', 'subsp._polymorphum', 'subsp._vincentii'],
+//     'Limosilactobacillus reuteri': ['clade_818','clade_938'],  // no abundance data
+//     'Peptostreptococcaceae_[G-7] [Eubacterium]_yurii': ['subsps._yurii_&_margaretiae','subsp._schtitka']
+// }
 //
 constants.anviserver_link = "https://vamps.mbl.edu/anviserver/pangenomes/"
 // this should reflect what is avalable on HOMD (vamps/anviserver)
@@ -407,5 +424,35 @@ constants.pangenomes = [
     },
 ]
 constants.PAGER_ROWS = 500
+
+constants.hmp_v3v5_to_suppress =[
+'34','36','851',
+'641','56','622','948','73','677','574','638','87','755','721','152','543','767','76' ,
+'114','116','612','834','127','128','141','550','591','601','598','682','764','275','283',
+'298','313','469','572','885','306','323','324','380','412','902','903','700','326','335','336',
+]
+
+// links on taxa description page
+constants.link_exceptions = {}
+constants.link_exceptions['105'] = {'ncbilink':'Eubacterium-infirmum','gcmlink':'Eubacterium%20infirmum','lpsnlink':'species/eubacterium-infirmum'}
+constants.link_exceptions['467'] = {'ncbilink':'Eubacterium-sulci','gcmlink':'Eubacterium%20sulci','lpsnlink':'species/eubacterium-sulci'}
+constants.link_exceptions['759'] = {'ncbilink':'Eubacterium-saphenum','gcmlink':'Eubacterium%20saphenum','lpsnlink':'species/eubacterium-saphenum'}
+constants.link_exceptions['673'] = {'ncbilink':'Eubacterium-minutum','gcmlink':'Eubacterium%20minutum','lpsnlink':'species/eubacterium-minutum'}
+constants.link_exceptions['694'] = {'ncbilink':'Eubacterium-nodatum','gcmlink':'Eubacterium%20nodatum','lpsnlink':'species/eubacterium-nodatum'}
+constants.link_exceptions['557'] = {'ncbilink':'Eubacterium-brachy','gcmlink':'Eubacterium%20brachy','lpsnlink':'species/eubacterium-brachy'}
+constants.link_exceptions['106'] = {'ncbilink':'Eubacterium-yurii','gcmlink':'Eubacterium%20yurii','lpsnlink':'subspecies/eubacterium-yurii-schtitka'}
+constants.link_exceptions['377'] = {'ncbilink':'Eubacterium-yurii','gcmlink':'Eubacterium%20yurii','lpsnlink':'subspecies/Eubacterium-yurii-margaretiae'}
+// constants.link_exceptions['174'] = {'lpsnlink':'family/Neisseriaceae'}
+// constants.link_exceptions['327'] = {'lpsnlink':'family/Neisseriaceae'}
+// constants.link_exceptions['274'] = {'lpsnlink':'order/Bacteroidales'}
+// constants.link_exceptions[''] = {'lpsnlink':''}
+// constants.link_exceptions[''] = {'lpsnlink':''}
+// constants.link_exceptions[''] = {'lpsnlink':''}
+// constants.link_exceptions[''] = {'lpsnlink':''}
+// constants.link_exceptions[''] = {'lpsnlink':''}
+// constants.link_exceptions[''] = {'lpsnlink':''}
+// constants.link_exceptions[''] = {'lpsnlink':''}
+// constants.link_exceptions[''] = {'lpsnlink':''}
+// constants.link_exceptions[''] = {'lpsnlink':''}
 
 module.exports = constants;
