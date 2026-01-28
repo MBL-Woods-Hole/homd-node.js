ca3f2173abcfa9fdc1d3b03a2902b61604750a9e
avoorhis <avoorhis@mbl.edu>
2026-01-28 07:42:01 -0500

    testing

---
+++ config/databaseORIG.js
@@ -0,0 +1,20 @@
+// db.js
+import mysql from 'mysql2';
+import 'dotenv/config';
+
+// Create the connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // Adjust based on your needs/server limits
+  queueLimit: 0,
+});
+
+// Get the Promise-wrapped pool for async/await usage
+const promisePool = pool.promise();
+
+// Export the pool instance to be used everywhere
+export default pool;
