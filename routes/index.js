4228bc13c36c5eb5b9b4d3ecd7ba981a1b1b3aa9
avoorhis <avoorhis@mbl.edu>
2026-05-18 15:26:01 -0400

    SS get fasta

--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,8 @@ router.get('/get_fasta', async function get_fasta(req, res) {
     // let q = "SELECT UNCOMPRESS(seq_compressed) as seq from PROKKA.ffn"
 //     let q = "SELECT UNCOMPRESS(seq_compressed) as seq from NCBI.faa"   //okay
 //     let q = "SELECT UNCOMPRESS(seq_compressed) as seq from NCBI.ffn"
-    q += " WHERE protein_id  in ('"+seqids.replace(/,/g, "','");+"')"
+    // prokka::ffn OKAY GCA_019602835.1_00003,GCA_027625375.1_00003,GCA_030503915.1_02746
+    q += " WHERE protein_id  in ('"+seqids.replace(/,/g, "','")+"')"
     
     console.log(q)
     try {
