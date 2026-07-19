96ecc0076be546e60b542784f6372841f780b6b3
avoorhis <avoorhis@mbl.edu>
2026-07-19 11:30:46 -0400

    testing pool

--- routes/queries.js
+++ routes/queries.js
@@ -88,20 +88,20 @@ export const get_db_updates_query = () => {
 export const get_annotation_query = (gid, anno) => {
   let q 
   if(anno === 'prokka'){
-    q = 'SELECT a.region, type, gc, a.protein_id, product, c.length_na, b.length_aa, `start`, `end`, gene'
+    q = "SELECT a.accession, type, gc, a.protein_id, product, '' as length_na, '' as length_aa, `start`, `end`, gene"
    q += ' FROM PROKKA.orf_gff a'
    q += ' LEFT JOIN PROKKA.faa b on a.genome_id=b.genome_id and a.protein_id=b.protein_id '
-   q += ' LEFT JOIN PROKKA.ffn c on a.genome_id=c.genome_id and a.protein_id=c.protein_id '
+   q += ' LEFT JOIN PROKKA.ffn c on a.genome_id=c.orf_id and a.protein_id=c.orf_id '
    q += " WHERE a.genome_id = '"+gid+"'"
     
     
 
   
   }else if(anno === 'ncbi') {
-    q = 'SELECT a.region, type, gc, a.protein_id, product, c.length_na, b.length_aa, `start`, `end`, gene'
+    q = "SELECT a.accession, type, gc, a.protein_id, product, '' as length_na, '' as length_aa, `start`, `end`, gene"
     q += ' FROM NCBI.orf_gff a'
     q += ' LEFT JOIN NCBI.faa b on a.genome_id=b.genome_id and a.protein_id=b.protein_id '
-    q += ' LEFT JOIN NCBI.ffn c on a.genome_id=c.genome_id and a.protein_id=c.protein_id '
+    q += ' LEFT JOIN NCBI.ffn c on a.genome_id=c.orf_id and a.protein_id=c.orf_id '
     q += " WHERE a.genome_id = '"+gid+"'"
   
   }else{ // BAKTA
