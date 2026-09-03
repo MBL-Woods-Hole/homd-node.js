4ae92054cc64dc4ac57f5392451028af0a42f72f
avoorhis <avoorhis@mbl.edu>
2026-09-03 17:05:19 -0400

    full text search

--- routes/index.js
+++ routes/index.js
@@ -554,8 +554,8 @@ router.post('/advanced_site_search_anno_mysql', async function advanced_site_sea
     })
 
     stream.on('end', () => {
-        console.log('Streaming finished.');
-        console.log('obj2',obj2)
+        //console.log('Streaming finished.');
+        //console.log('obj2',obj2)
         res.render('pages/full_site_search_results', {
             title: 'HOMD :: Search Results',
             pgname: '', // for AboutThisPage 
